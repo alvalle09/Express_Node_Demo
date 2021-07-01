9456e32bd36ed2da551c27988e913c7423722be5
alvalle09 <phredvalle@gmail.com>
2021-07-01 15:54:16 -0500

    add an api/courses route

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ app.get('/', (req, res) => {
     res.send('Hello, mundo!');
 });
 
+app.get('/api/courses', (req, res) => {
+    res.send([1, 2, 3]);
+});
+
 app.listen(3000, () => console.log('Listening on port 3000!'));

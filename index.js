5064943d1c9c1c84d304fbe39fcc2cc4de8fa3fc
alvalle09 <phredvalle@gmail.com>
2021-07-06 23:01:33 -0500

    pull single course from api

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello, mundo!');
+    res.send('Hello, Node & Express!!!!!');
 });
 
 app.get('/api/courses', (req, res) => {
     res.send([1, 2, 3]);
 });
 
-app.listen(3000, () => console.log('Listening on port 3000!'));
+app.get('/api/courses/:id', (req, res) => {
+    res.send(req.params.id);
+})
+
+// PORT
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));

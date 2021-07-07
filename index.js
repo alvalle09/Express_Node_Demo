5184ca5dfc07db4d16a28f3dfb0cceccab6ee42d
alvalle09 <phredvalle@gmail.com>
2021-07-06 23:35:34 -0500

    handle params query  and 404s

--- index.js
+++ index.js
@@ -2,19 +2,29 @@ const express = require('express');
 
 const app = express();
 
+const courses = [
+    { id: 1, name: 'course1' },
+    { id: 2, name: 'course2' },
+    { id: 3, name: 'course3' },
+    { id: 4, name: 'course4' }
+];
+
 app.get('/', (req, res) => {
     res.send('Hello, Node & Express!!!!!');
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3]);
+    res.send(courses);
 });
 
 app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('The course with given id not found');  
+    res.send(course);
     res.send(req.params.id);
 })
 
-app.get('/api/posts/:year/:month', (req, res) => {
+app.get('/api/courses/:id', (req, res) => {
     res.send(req.query);
 })
 
